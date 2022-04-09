6cb52222fa3e271c96d7f05dad1e4b152bbe90f9
Sameera Perera <sameera.man@gmail.com>
2022-04-09 23:10:15 +0800

    dcpromo updates

--- ServerBootstrap/windows_server_dcpromo.ps1
+++ ServerBootstrap/windows_server_dcpromo.ps1
@@ -1,5 +1,7 @@
 Param (
-    [string]$domainname
+    [string]$domainname="contoso.com",
+    [string]$baseDN="DC=contoso,DC=com",
+    [string]$dcpassword
 )
 
 # Run the Server base bootstrap script
@@ -14,6 +16,23 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
 Install-windowsfeature -name DNS -IncludeManagementTools
 
-
+$password = ConvertTo-SecureString $dcpassword -AsPlainText -Force
 Import-Module ADDSDeployment
-Install-ADDSForest -CreateDnsDelegation:$false -DomainMode "WinThreshold" -DomainName $domainname -ForestMode "WinThreshold" -InstallDns:$true -NoRebootOnCompletion:$false -Force:$true 
+Install-ADDSForest -CreateDnsDelegation:$false -DomainMode "WinThreshold" -DomainName $domainname -ForestMode "WinThreshold" -InstallDns:$true -NoRebootOnCompletion:$true -SafeModeAdministratorPassword $password -Force:$true 
+
+
+# Create the OU's
+New-ADOrganizationalUnit -Name "Managed Users" -Path $baseDN -ProtectedFromAccidentalDeletion $False -Description "Managed User Accounts"
+New-ADOrganizationalUnit -Name "Managed Computers" -Path $baseDN -ProtectedFromAccidentalDeletion $False -Description "Managed Computers"
+
+# Create the user accounts
+New-ADUser -Name "Donald Trump" -GivenName "Donald" -Surname "Trump" -SamAccountName "donald.trump" -UserPrincipalName "donald.trump@$domainname" -Path "OU=Managed Users,$baseDN" -AccountPassword $password  -Enabled $true -PasswordNeverExpires $true
+New-ADUser -Name "Katy Perry" -GivenName "Katy" -Surname "Perry" -SamAccountName "katy.perry" -UserPrincipalName "katy.perry@$domainname" -Path "OU=Managed Users,$baseDN" -AccountPassword $password  -Enabled $true -PasswordNeverExpires $true
+
+# Create Service Accounts
+New-ADUser -Name "SQL Service" -GivenName "SQL" -Surname "Service" -SamAccountName "sql.svc" -UserPrincipalName "sql.svc@$domainname" -Path "CN=Managed Service Accounts,$baseDN" -AccountPassword $password  -Enabled $true -PasswordNeverExpires $true
+
+
+
+
+shutdown -r -t 10
